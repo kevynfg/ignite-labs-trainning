68df03d7e7d1bf7c4677dc906ddc9319cfbedc8b
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-06-29 18:14:17 -0300

    add: background images, subcriber feature and router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Event } from './pages/Event'
+import { Subscribe } from './pages/Subscribe'
 
 export function Router() {
     return (
         <Routes>
             <Route path="/" element={
-              <h1>Home</h1>  
+              <Subscribe /> 
             } />
             <Route path="/event" element={
               <Event />
@@ -13,6 +14,7 @@ export function Router() {
             <Route path="/event/lesson/:slug" element={
               <Event />
             } />
+
         </Routes>
     )
 }
