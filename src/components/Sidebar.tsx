18b092048dbde38d9db47e962b9c2d9dedcfc8a3
Kevyn Gonçalves <kevynredlotus@gmail.com>
2022-06-29 19:40:44 -0300

    add: graphql codegen

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,6 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { Lesson } from "./Lesson";
-
-const GET_LESSONS_QUERY = gql`
-    query {
-        lessons(orderBy: availableAt_ASC, stage: PUBLISHED) {
-            id
-            slug
-            title
-            availableAt
-        }
-    }
-`
+import { useGetLessonsQuery } from '../graphql/generated'
 
 interface GetLessonsQueryResponse {
     lessons: Array<{
@@ -23,7 +13,7 @@ interface GetLessonsQueryResponse {
 }
 
 export function Sidebar() {
-    const {data, error} = useQuery<GetLessonsQueryResponse>(GET_LESSONS_QUERY)
+    const {data, error} = useGetLessonsQuery();
     return (
         <aside className="w-[348px] bg-gray-700 p-6 border-l border-gray-600">
             <span className="font-bold text-2xl pb-6 mb-6 border-b border-gray-500 block">Cronograma de aulas</span>
